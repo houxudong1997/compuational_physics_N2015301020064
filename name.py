f838f4ff5093f090d9db0ab52c52094b09469e54
houxudong1997 <31854395+houxudong1997@users.noreply.github.com>
2017-09-12 22:15:00 +0800

    Add files via upload

---
+++ name.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 12 21:14:14 2017
+
+@author: Administrator
+"""
+
+#name.py
+print("#      #                            #         #                   #                                         ")
+print("#      #                             #       #                    #                                         ")
+print("#      #                              #     #                     #                                         ")
+print("#      #                               #   #                      #                                         ")
+print("#      #                                # #                       #                                         ")
+print("########      # #     #     #            #       #     #     # #  #      # #      #  # #       # # #        ")
+print("#      #    #     #   #     #           # #      #     #    #     #    #     #    # #    #    #     #       ")
+print("#      #   #       #  #     #          #   #     #     #   #      #   #       #   #      #   #       #      ")
+print("#      #   #       #  #     #         #     #    #     #   #      #   #       #   #      #   #        #     ")
+print("#      #    #     #   #    #         #       #   #    #     #     #    #     #    #      #    #       #     ")
+print("#      #      # #      # #          #         #   # #        #  #        # #      #      #     #  # # #     ")
+print("                                                                                                      #     ")
+print("                                                                                                      #     ")
+print("                                                                                                      #     ")
+print("                                                                                               #      #     ")
+print("                                                                                                # # #       ")
