85d82423df6b2d1b2a9e609743271d5ca56d111a
houxudong1997 <31854395+houxudong1997@users.noreply.github.com>
2017-09-30 00:29:56 +0800

    Add files via upload

---
+++ Euler mothed.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 22:59:44 2017
+
+@author: Administrator
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+list1=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+list2=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+v = 40
+h = 1
+d = v * h
+i = 0
+j = 1
+while j != 11:
+    list1[j] = list1[i] + h
+    list2[j] = list2[i] + d
+    i += 1
+    j += 1
+T = np.array(list1)
+X = np.array(list2)
+plt.plot(T,X,marker='o',mec='b',mfc='y',color='r')
+plt.xlabel('t (s)')
+plt.ylabel('x (m)')
+plt.title('line of x-t')
+plt.show()
